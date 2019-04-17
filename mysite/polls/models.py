28698a9bbba719a6ca027b3a66e3d31be2be04cb
Vanessa <vanessawijono@yahoo.com>
2019-04-17 20:11:30 +0200

    Writing your first Django app, part 7 https://docs.djangoproject.com/en/2.0/intro/tutorial07/

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -15,6 +15,14 @@ class Question(models.Model):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+
 
 # A Choice has two fields: the text of the choice and a vote tally. Each Choice is associated with a Question.
 class Choice(models.Model):
